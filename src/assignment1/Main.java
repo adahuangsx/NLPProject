552b275a7ef8596e0b4efe96c544d47bdf55590b
adahuangsx <huangsx_97@foxmail.com>
2020-09-21 18:22:55 -0400

    Solved Infinity problem

--- src/assignment1/Main.java
+++ src/assignment1/Main.java
@@ -144,6 +144,7 @@ public class Main {
 			List<Double> likelihood_tri = calculateLineProbwithTrigramModel(new StringBuilder(sb), addk);
 			double crtProb_tri = meanProbability(likelihood_tri);
 			double crtPerp_tri = meanPerplexity(likelihood_tri);
+//			System.out.print(crtPerp_tri + " ");
 			prob_uni.add(crtProb_uni);
 			perp_uni.add(crtPerp_uni);
 			prob_bi.add(crtProb_bi);
@@ -151,29 +152,36 @@ public class Main {
 			prob_tri.add(crtProb_tri);
 			perp_tri.add(crtPerp_tri);
 		}
+		
 		System.out.println("Test score:");
-		System.out.println("(Unigram, add:" + addk + ") Prob.  Perp.");
-		System.out.println(meanProbability(prob_uni) + "  " + meanProbability(perp_uni));
-		System.out.println("(Bigram, add:" + addk + ") Prob.  Perp.");
-		System.out.println(meanProbability(prob_bi) + "  " + meanProbability(perp_bi));
-		System.out.println("(Trigram, add:" + addk + ") Prob.  Perp.");
-		System.out.println(meanProbability(prob_tri) + "  " + meanProbability(perp_tri));
+		System.out.println("(Unigram, add:" + addk + ") Prob " + meanProbability(prob_uni) + " Perp." + mean(perp_uni));
+		System.out.println("(Bigram, add:" + addk + ") Prob " + meanProbability(prob_bi) + " Perp." + mean(perp_bi));
+		System.out.println("(Trigram, add:" + addk + ") Prob " + meanProbability(prob_tri) + " Perp." + mean(perp_tri));
 		reader.close();
 	}
 	private double meanProbability(List<Double> llhd) {
-		double res = 0;
+		double res = 0.0;
 		for (double prob : llhd) {
-			res += prob;
+			res += prob; 
 		}
 		return res / llhd.size();
 	}
-	private double meanPerplexity(List<Double> llhd) {
-		double res = 1;
+	private double mean(List<Double> llhd) {
+		double res = 0.0;
 		for (double prob : llhd) {
-			if (prob != 0) // this can be avoided by add-one
-				res *= prob;
+			res += prob; 
+//			System.out.print(res + " ");
 		}
-		return Math.pow(res, -1.0 / llhd.size());
+//		System.out.println("res: " + res + ", " + llhd.size());
+		return res / llhd.size();
+	}
+	private double meanPerplexity(List<Double> llhd) {
+		double res = 1.0;
+		for (double prob : llhd) {
+			if (prob > 0.0) // this can be avoided by add-one
+				res *= Math.pow(prob, -1.0 / llhd.size());
+		}
+		return res;
 	}
 	
 	public List<Double> calculateLineProbwithUnigramModel(StringBuilder sb) {
@@ -181,7 +189,7 @@ public class Main {
 		for (char c : sb.toString().toCharArray()) {
 			String uni = String.valueOf(c);
 			if (lm.get(uni) == null) {
-				likelihood.add(0.0);
+				likelihood.add(Double.MIN_VALUE);
 			} else {
 				likelihood.add(1.0 * lm.get(uni) / sizeOfUniword);
 			}
@@ -190,14 +198,14 @@ public class Main {
 	}
 	public List<Double> calculateLineProbwithBigramModel(StringBuilder sb, double addk) {
 		List<Double> likelihood = new ArrayList<>();
-//		likelihood.add(1.0 * (lm.get(sb.substring(0, 1)) + addk) / (sizeOfUniword) + addk * this.V);
+		likelihood.add(1.0 * (lm.get(sb.substring(0, 1)) + addk) / (sizeOfUniword) + addk * this.V);
 		while (sb.length() > 2) {
 			String bi = sb.substring(0, 2);
 			String uni = bi.substring(0, 1);
 			int countBi = lm.get(bi) == null ? 0 : lm.get(bi);
 			int countUni = lm.get(uni) == null ? 0 : lm.get(uni);
 			if (addk == 0 && (countUni == 0 || countBi == 0)) {
-				likelihood.add(0.0);
+				likelihood.add(Double.MIN_VALUE);
 			} else {
 				likelihood.add(1.0 * (countBi + addk) / (countUni + addk * this.V));				
 			}
@@ -219,8 +227,9 @@ public class Main {
 			int countBi = lm.get(bi) == null ? 0 : lm.get(bi);
 			int countTri = lm.get(tri) == null ? 0 : lm.get(tri);
 			if (addk == 0 && (countBi == 0 || countTri == 0)) {
-				likelihood.add(0.0);
+				likelihood.add(Double.MIN_VALUE);
 			} else {
+				if (1.0 * (countTri + addk) / (countBi + addk * this.V) == 0.0) {System.out.println("=====");}
 				likelihood.add(1.0 * (countTri + addk) / (countBi + addk * this.V));
 			}
 			sb.deleteCharAt(0);
@@ -230,79 +239,41 @@ public class Main {
 	public void checkValid(String history) {
 		int historyCount = lm.get(history);
 		List<Integer> counts = new ArrayList<>();
-		int total = 0;
 		for (Map.Entry<String, Integer> entry : this.tri.entrySet()) {
 			if (entry.getKey().startsWith(history)) {
 				counts.add(entry.getValue());
 				System.out.print(entry.getKey() + " ");
-				total += entry.getValue();
 			}
 		}
 		double prob = 0;
 		for (int count : counts) {
-			prob += 1.0 * count / total;
+			prob += 1.0 * count / historyCount;
 		}
 		System.out.println("The total prob. of prefix " + history + " is " + prob);
 	}
 	
-	public double calculateScoreWithTrigramModel(Map<String, Integer> bigram, Map<String, Integer> trigram, String fileName) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(fileName));
-		String line = null;
-		StringBuilder sb = new StringBuilder();
-		while ((line = reader.readLine()) != null) {
-			for (char c : line.toCharArray()) {
-				if (Character.isLetter(c)) {
-					if (!Character.isLowerCase(c)) {
-						c = (char) (c - 'A' + 'a');
-					}
-					sb.append(c);
-				}
-				else if (sb.charAt(sb.length() - 1) != '$'){
-					sb.append('$');
-				}
-			}
-		}
-		// sb is processed into "abcds$aa$fdja ..."
-		double logScore = 0;
-//		double score = 1.0;
-		while (sb.length() >= 3) {
-			String crtTri = sb.substring(0, 3);
-			String crtBi = crtTri.substring(0, 2);
-			if (bigram.get(crtBi) == null || trigram.get(crtTri) == null) {
-//				System.out.println("Missing: " + crtTri + ", " + crtBi);
-			}
-			else {
-				logScore = logScore + Math.log(trigram.get(crtTri)) - Math.log(bigram.get(crtBi));
-//				System.out.println(crtBi + ", " + bigram.get(crtBi) + ", " + crtTri + ", " + trigram.get(crtTri));
-//				score = score * bigram.get(crtBi) / unigram.get(crtC); // MLE
-			}
-			sb.deleteCharAt(0);
-		}
-		reader.close();
-		return logScore;
-	}
 	
 	
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Main t = new Main();
-		t.training(enTrainingDir);
-		t.checkValid("AB");
+//		t.training(enTrainingDir);
+//		t.checkValid("AB");
 		
-//		t.training(enTrainingDir);
-//		t.calculateScore(testDir, 0, null);
-//		t.training(esTrainingDir);
-//		t.calculateScore(testDir, 0, null);
-//		t.training(deTrainingDir);
-//		t.calculateScore(testDir, 0, null);
-//		
-//		t.training(enTrainingDir);
-//		t.calculateScore(testDir, 1, null);
-//		t.training(esTrainingDir);
-//		t.calculateScore(testDir, 0, null);
-//		t.training(deTrainingDir);
-//		t.calculateScore(testDir, 0, null);
+		t.training(enTrainingDir);
+		t.calculateScore(testDir, 0, null);
+		t.training(esTrainingDir);
+		t.calculateScore(testDir, 0, null);
+		t.training(deTrainingDir);
+		t.calculateScore(testDir, 0, null);
+		double k = 1;
+		t.training(enTrainingDir);
+		t.calculateScore(testDir, k, null);
+		t.training(esTrainingDir);
+		t.calculateScore(testDir, k, null);
+		t.training(deTrainingDir);
+		t.calculateScore(testDir, k, null);
 	}
 
 }
